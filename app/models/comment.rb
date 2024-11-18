a0f494b93826ecccb194a6a75a195b754a3b75e4
nicolaspark1 <141599069+nicolaspark1@users.noreply.github.com>
2024-11-18 05:38:14 +0000

    added associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -17,7 +17,8 @@ class Comment < ApplicationRecord
   
   ## Direct associations
 
-  # Comment#commenter: returns a row from the users table associated to this comment by the author_id column
+  # Comment#commenter: returns a row from the users table associated to this comment by the author_id column\
+  
 
   # Comment#photo: returns a row from the photos table associated to this comment by the photo_id column
 end
